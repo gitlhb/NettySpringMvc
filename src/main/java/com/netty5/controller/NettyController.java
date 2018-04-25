b7ab173c420dd7b0c4f833b0daf6f4e24a299396
gitliuhongbing <liuhb@guahao.com>
2018-04-25 10:32:23 +0800

    test

--- src/main/java/com/netty5/controller/NettyController.java
+++ src/main/java/com/netty5/controller/NettyController.java
@@ -12,7 +12,6 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 
-
 import io.netty.bootstrap.Bootstrap;
 import io.netty.channel.Channel;
 import io.netty.channel.ChannelFuture;
@@ -27,65 +26,66 @@ import io.netty.handler.codec.string.StringEncoder;
 
 @Controller
 public class NettyController {
-	@ResponseBody
-	@RequestMapping(value = "/netty", method = RequestMethod.POST)
-	public Map<String, String> postMe(@RequestParam("name") String instr) {
-		Map<String, String> map = new HashMap<String, String>();
-		System.out.println("inStr:" + instr);
-		try {
-			ClientHandler c = new ClientHandler();
-			String handler = sendMessage("127.0.0.1", 11111, instr, c);
-			map.put("code", "0");
-			map.put("msg", handler);
-		} catch (Exception e) {
-			map.put("code", "0");
-			map.put("msg", e.getMessage());
-			e.printStackTrace();
-		}
-		System.out.println("map:" + map.toString());
-		return map;
-	}
+    @ResponseBody
+    @RequestMapping(value = "/netty", method = RequestMethod.POST)
+    public Map<String, String> postMe(@RequestParam("name") String instr) {
+        Map<String, String> map = new HashMap<String, String>();
+        System.out.println("inStr:" + instr);
+        try {
+            ClientHandler c = new ClientHandler();
+            String handler = sendMessage("127.0.0.1", 11111, instr, c);
+            map.put("code", "0");
+            map.put("msg", handler);
+        } catch (Exception e) {
+            map.put("code", "0");
+            map.put("msg", e.getMessage());
+            e.printStackTrace();
+        }
+        System.out.println("map:" + map.toString());
+        return map;
+    }
 
-	public static String sendMessage(final String hostAddress, final int port, String request,
-			final ClientHandler chand) {
-		String message="";
-		Bootstrap client = new Bootstrap();
-		EventLoopGroup group = new NioEventLoopGroup();
-		ChannelFuture future = null;
-		try {
-			client.group(group);
-			client.option(ChannelOption.SO_KEEPALIVE, Boolean.TRUE);
-			client.option(ChannelOption.TCP_NODELAY, Boolean.TRUE);
-			client.channel(NioSocketChannel.class);
-			client.handler(new ChannelInitializer<Channel>() {
+    public static String sendMessage(final String hostAddress, final int port, String request,
+                                     final ClientHandler chand) {
+        String message = "";
+        Bootstrap client = new Bootstrap();
+        EventLoopGroup group = new NioEventLoopGroup();
+        ChannelFuture future = null;
+        try {
+            client.group(group);
+            client.option(ChannelOption.SO_KEEPALIVE, Boolean.TRUE);
+            client.option(ChannelOption.TCP_NODELAY, Boolean.TRUE);
+            client.channel(NioSocketChannel.class);
+            client.handler(new ChannelInitializer<Channel>() {
 
-				@Override
-				protected void initChannel(Channel ch) throws Exception {
-					ChannelPipeline pipeline = ch.pipeline();
-					pipeline.addLast(new StringDecoder(Charset.defaultCharset()));
-					pipeline.addLast(new StringEncoder(Charset.defaultCharset()));
-					pipeline.addLast(chand);
-				}
-			});
-			future = client.connect(hostAddress, port).sync();
-			if (future.isSuccess()) {
-				System.out.println("客户端连接成功，并向服务端发送消息:"+request);
-				ChannelFuture future2 = future.channel().writeAndFlush(request).sync();
-				System.out.println("客户端发送数据是否成功:" + future2.isSuccess());
-			}
-		} catch (Exception e) {
-		}
-		try {
-			Thread.sleep(200);
-			message=chand.getMessage();
-			if (future != null) {
-				//System.out.println("是否关闭成功:"+future.channel().closeFuture().sync().isSuccess());
-			}
-			group.shutdownGracefully();
-		} catch (InterruptedException e) {
-			e.printStackTrace();
-		}
+                @Override
+                protected void initChannel(Channel ch) throws Exception {
+                    ChannelPipeline pipeline = ch.pipeline();
+                    pipeline.addLast(new StringDecoder(Charset.defaultCharset()));
+                    pipeline.addLast(new StringEncoder(Charset.defaultCharset()));
+                    pipeline.addLast(chand);
+                }
+            });
+            future = client.connect(hostAddress, port).sync();
+            if (future.isSuccess()) {
+                System.out.println("客户端连接成功，并向服务端发送消息:" + request);
+                ChannelFuture future2 = future.channel().writeAndFlush(request).sync();
+                System.out.println("客户端发送数据是否成功:" + future2.isSuccess());
+            }
+        } catch (Exception e) {
+        }
+        try {
+            Thread.sleep(50);
+            message = chand.getMessage();
 
-		return message;
-	}
+            if (future != null) {
+                //System.out.println("是否关闭成功:"+future.channel().closeFuture().sync().isSuccess());
+            }
+            group.shutdownGracefully();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return message;
+    }
 }
